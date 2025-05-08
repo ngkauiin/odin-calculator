45bbe1a03b007a58b2ae5fadc9d6cea7f4b42c2a
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-05-08 20:21:17 +1000

    add more if statement to case 2; add case 3 to function if the input is digits or '='

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let numberA = '';
-let numberB = '';
-let operator;
+let numberA = '0';
+let numberB = '0';
+let operator = '';
 let input = '';
 
 function add(a,b) {
@@ -43,12 +43,18 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const userInput = button.className;
-    // check if input is digit number, if so then add that to numberA (stage 0)
-    // if the input changed to operator, keep the numberA and store the operator (stage 0>1)
-    // if the following input is also operator, keep changing the operator (stage 1)
-    // if the following input is a digit number, store the operator and start storing that input as numberB (stage 1>2)
-    // if the following input is a operator, keep the numberB (stage 2>3)
-    // in stage 3, operate the math and display the value on HTML (stage 3>0)
+    // (stage 0) check if input is digit number, if so then add that to numberA
+    // (stage 0>1) if the input changed to operator, keep the numberA and store the operator 
+    // (stage 1) if the following input is also operator, keep changing the operator
+    // (stage 1>2) if the following input is a digit number, store the operator and start storing that input as numberB
+    // (stage 2) if the following input is a operator, store the numberB
+    // (stage 2) store the result as numberA, operate the math and display the value on HTML
+    // (stage 2) check if last operator !== '=', save the last operator and clear numberB and move to stage 1
+    // (stage 3) if operator==='=', go to stage 3
+    // (stage 3) if the next input is digits, CLEAR and go to stage 0
+    // (stage 3) if the next input is a oeprator (not =), clear numberB, store the operator and go to stage 1
+    // (stage 3) if the next input is also '=', do the operate(a,b,x) and display with the previous operator
+
     switch (stage) {
       case 0:
         if(parseInt(userInput)) {
@@ -72,11 +78,25 @@ buttons.forEach((button) => {
         } else {
           numberA = operate(numberA,numberB,operator);
           display(numberA);
-          operator = userInput;
-          stage++;
+          if(userInput!=='=') {
+            operator = userInput;
+            numberB = '0';
+            stage = 1;
+          } else if (userInput==='=') {
+            stage = 3;
+          }
         }
         break;
       case 3:
+        if (parseInt(userInput)) {
+          numberA = userInput;
+          numberB = '0';
+          operator = '';
+          stage = 0;
+        } else if (userInput === '=') {
+          numberA = operate(numberA,numberB,operator);
+          display(numberA);
+        }
         break;
     }
   })
